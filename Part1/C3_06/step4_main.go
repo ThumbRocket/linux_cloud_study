0a01d66f2cb3ad74941e7d24bbcede14139cfdb1
ThumbRocket <djawlgh20@gmail.com>
2024-09-07 16:43:36 +0900

    fix folder name

---
+++ Part1/C3_06/step4_main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// docker           run image <CMD> <ARG>
+// go run main.go   run       <CMD> <ARG>
+
+// Step4: 컨테이너 환경 시작시 호스트명을 container로 변경.
+// $ go run . run /bin/sh
+// $ hostname
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		os.Exit(1)
+	}
+}
+
+func run() {
+	fmt.Printf("Running: %v\n", os.Args[2:])
+	// cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	//proc/self/exe -> golang 실행 프로그램에 child 인자 설정
+	// 자기 자신에 대해 child를 만들어 arg 전달
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running child: %v\n", os.Args[2:])
+
+	// hostname 설정
+	syscall.Sethostname([]byte("container"))
+	// hostname 변경하는 부분
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
